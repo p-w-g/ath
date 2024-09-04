97b8ee6f91aa2db5893c811ef3c17aeb9309ffb4
p-w-g <p.grzelczyk@gmail.com>
2024-09-04 21:22:30 +0200

    add skip/only flags

--- commands/FEP.cs
+++ commands/FEP.cs
@@ -6,14 +6,34 @@ namespace ath.commands
     {
       if (args.Length == 0)
       {
-        Console.WriteLine("Usage: ath fep <command>");
+        Console.WriteLine("Usage: ath fep <command> [--skip-folder-folder || --only-folder-folder]");
         return;
       }
-      string innerCommand = args[0];
-      string innerCommandArgs = args.Length > 1 ? string.Join(" ", args[1..]) : string.Empty;
+      var filteredArgs = args.Where(arg => !arg.Contains("--skip-") && !arg.Contains("--only-")).ToArray();
+      if (filteredArgs.Length == 0)
+      {
+        Console.WriteLine("Usage: ath fep <command> [--skip-folder-folder || --only-folder-folder]");
+        return;
+      }
+
+      bool skip = args.Any(arg => arg.StartsWith("--skip"));
+      bool only = args.Any(arg => arg.StartsWith("--only"));
+
+      string[] skippedFlag = skip && args != null ? Array.Find(args, arg => arg.Contains("--skip-"))!.Split("--skip-")[1].Split("-") : [];
+      string[] onlyFlag = only && args != null ? Array.Find(args, arg => arg.Contains("--only-"))!.Split("--only-")[1].Split("-") : [];
+
+      string innerCommand = filteredArgs[0];
+      string innerCommandArgs = filteredArgs.Length > 1 ? string.Join(" ", filteredArgs[1..]) : string.Empty;
+
       string[] directories = Directory.GetDirectories(Directory.GetCurrentDirectory());
 
-      var tasks = directories.Select(dir => Task.Run(() => cliTooling.RunCommand(innerCommand, innerCommandArgs, dir))).ToArray();
+      string[] filteredDirectories = skip
+      ? directories.Where(dir => skippedFlag.Any(flag => !dir.Contains(flag))).ToArray()
+      : only
+      ? directories.Where(dir => onlyFlag.Any(flag => dir.Contains(flag))).ToArray()
+      : directories;
+
+      var tasks = filteredDirectories.Select(dir => Task.Run(() => cliTooling.RunCommand(innerCommand, innerCommandArgs, dir))).ToArray();
       await Task.WhenAll(tasks);
     }
   }
--- commands/Help.cs
+++ commands/Help.cs
@@ -7,6 +7,8 @@ namespace ath.commands
       Console.WriteLine("Available commands:");
       Console.WriteLine("  help    - Show help information");
       Console.WriteLine("  fep     - Run command for nested folders in CWD.");
+      Console.WriteLine("            takes a list of optional folders to either skip or run command in, separated by -");
+      Console.WriteLine("            ath fep <<command>> [--skip-foo-bar-baz || --only-gris-gras-gres]");
     }
 
   }
