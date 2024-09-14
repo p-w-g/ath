3972b8702040b5cb2965dda2ec84559835c6fe5f
p-w-g <p.grzelczyk@gmail.com>
2024-09-14 15:39:45 +0200

    describe commands

--- commands/Help.cs
+++ commands/Help.cs
@@ -11,6 +11,9 @@ namespace ath.commands
                 takes a list of optional folders to either skip or run command in, separated by '-'
                 ath fep <<command>> [--skip-foo-bar-baz || --only-gris-gras-gres]
         pcp     prints out config file's path
+        swd     sets current working directory as a default working directory for future
+                uses with fep, untill it gets unset or new directory is set
+        uwd     removes default working directory and allows running fep in current working directory
             ";
             Console.WriteLine(HelpText);
         }
