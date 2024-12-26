f1bd6db0a06b50cd0f236e169fb12d0fc6c37ad4
p-w-g <p.grzelczyk@gmail.com>
2024-12-26 17:31:29 +0100

    overload taskrunners RunCommand to migrate FEP to instance object

--- cliUtils/TaskRunner.cs
+++ cliUtils/TaskRunner.cs
@@ -77,72 +77,76 @@ namespace ath.commands
             }
         }
 
-        //     internal static async Task RunCommand(Dictionary<string, string[]> optionsObject)
-        //     {
-        //         string shell =
-        //             Environment.OSVersion.Platform == PlatformID.Win32NT ? "cmd.exe" : "/bin/bash";
-        //         string shellArguments =
-        //             Environment.OSVersion.Platform == PlatformID.Win32NT
-        //                 ? $"/c {command} {arguments}"
-        //                 : $"-c \"{command} {arguments}\"";
+        internal static async Task RunCommand(
+            Dictionary<string, string[]> Instance,
+            string workingDirectory = ""
+        )
+        {
+            string command = string.Join(" ", Instance["PayLoad"]);
+            string shell =
+                Environment.OSVersion.Platform == PlatformID.Win32NT ? "cmd.exe" : "/bin/bash";
+            string shellArguments =
+                Environment.OSVersion.Platform == PlatformID.Win32NT
+                    ? $"/c {command}"
+                    : $"-c \"{command}\"";
 
-        //         var processInfo = new ProcessStartInfo(shell, shellArguments)
-        //         {
-        //             RedirectStandardOutput = true,
-        //             RedirectStandardError = true,
-        //             UseShellExecute = false,
-        //             CreateNoWindow = true,
-        //             WorkingDirectory = workingDirectory,
-        //         };
+            var processInfo = new ProcessStartInfo(shell, shellArguments)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = workingDirectory,
+            };
 
-        //         Console.WriteLine($"Running '{command} {arguments}' in {workingDirectory}");
+            Console.WriteLine($"Running '{command}' in {workingDirectory}");
 
-        //         using Process process = new() { StartInfo = processInfo };
+            using Process process = new() { StartInfo = processInfo };
 
-        //         try
-        //         {
-        //             process.Start();
+            try
+            {
+                process.Start();
 
-        //             Task<string> output = process.StandardOutput.ReadToEndAsync();
-        //             Task<string> error = process.StandardError.ReadToEndAsync();
+                Task<string> output = process.StandardOutput.ReadToEndAsync();
+                Task<string> error = process.StandardError.ReadToEndAsync();
 
-        //             if (sustain)
-        //             {
-        //                 process.WaitForExit();
-        //             }
-        //             else
-        //             {
-        //                 bool exited = await Task.Run(() => process.WaitForExit(300000));
-        //                 if (!exited)
-        //                 {
-        //                     Console.WriteLine(
-        //                         $"Command timed out in {workingDirectory} after 5 minutes."
-        //                     );
-        //                     process.Kill();
-        //                     return;
-        //                 }
-        //             }
+                // Timeouts, Sustain and Config
+                // if (sustain)
+                // {
+                //     process.WaitForExit();
+                // }
+                // else
+                // {
+                // bool exited = await Task.Run(() => process.WaitForExit(300000));
+                // if (!exited)
+                // {
+                //     Console.WriteLine($"Command timed out in {workingDirectory} after 5 minutes.");
+                //     process.Kill();
+                //     return;
+                // }
+                // }
 
-        //             if (process.ExitCode == 0)
-        //             {
-        //                 Console.WriteLine($"Command executed successfully in {workingDirectory}.");
-        //                 Console.WriteLine(await output);
-        //             }
-        //             else
-        //             {
-        //                 Console.WriteLine($"Command failed gracefully in {workingDirectory}.");
-        //                 Console.WriteLine(await error);
-        //             }
-        //         }
-        //         catch (Exception e)
-        //         {
-        //             Console.WriteLine($"Caught exception in {workingDirectory} with error:");
-        //             Console.WriteLine(e);
-        //         }
-        //         finally
-        //         {
-        //             process.Dispose();
-        //         }
-        //     }
+                process.WaitForExit();
+                if (process.ExitCode == 0)
+                {
+                    Console.WriteLine($"Command executed successfully in {workingDirectory}.");
+                    Console.WriteLine(await output);
+                }
+                else
+                {
+                    Console.WriteLine($"Command failed gracefully in {workingDirectory}.");
+                    Console.WriteLine(await error);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Caught exception in {workingDirectory} with error:");
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                process.Dispose();
+            }
+        }
     }
 }
